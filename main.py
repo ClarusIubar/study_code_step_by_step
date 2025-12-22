f47219678df579fb167823785286a5f55523178e
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-22 14:48:30 +0900

    __repr__ 추가

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ class Member:
     # __str__은 객체를 print() 함수로 출력할 때 어떻게 보여줄지 정의하는 함수 
     # 사람이 읽기 편한 형태로 리턴하도록 설정합니다.
     def __str__(self):
-        return f"Member(이름: {self.name}, 나이: {self.age})"
+        return f"Member(name: {self.name}, age:{self.age})"
+    
+    # 리스트에 담기거나, 객체의 '본모습'을 표현할 때
+    def __repr__(self):
+        return f"Member(name='{self.name}', age={self.age})"
 
 test = Member("공욱재", 24)
-print(test) # Member(이름: 공욱재, 나이: 24)
+print(test) # Member(name: 공욱재, age: 24)
+
+members = [test] # test 인스턴스를 단순히 리스트로 감싸서 테스트
+print(members) # [Member(name='공욱재', age=24)]
