7036cda3b04cda5e280758c9fe927d57a4e4497b
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-22 13:07:32 +0900

    멤버 클래스로 변경

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-member = [ 
-    {
-    "name" : "공욱재",
-    "age" : 24,
-    "home":"대전",
-    },
-    {
-    "name" : "고경명",
-    "age" : 34,
-    "home":"대전",
-    },
-    {
-    "name" : "김노현",
-    "age" : 24,
-    "home":"대전",
-    },
-]
+class Member:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+test = Member("공욱재", 24)
+print(test)
