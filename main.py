961cc3d8829820d579488e4eea56b175776db27f
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-22 14:35:41 +0900

    __str__ 추가

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ class Member:
         self.name = name
         self.age = age
 
+    # __str__은 객체를 print() 함수로 출력할 때 어떻게 보여줄지 정의하는 함수 
+    # 사람이 읽기 편한 형태로 리턴하도록 설정합니다.
+    def __str__(self):
+        return f"Member(이름: {self.name}, 나이: {self.age})"
+
 test = Member("공욱재", 24)
-print(test)
+print(test) # Member(이름: 공욱재, 나이: 24)
