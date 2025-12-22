ebe10eb35c72a1550323e8e6e9ae5841e27d8505
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-22 16:55:11 +0900

    pydantic.dataclass로 변환 및 Annotated로 데이터 검증

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from dataclasses import dataclass
+from typing import Annotated
+from pydantic.dataclasses import dataclass
+from pydantic import Field, AfterValidator
 
+# 1. 재사용 가능한 데이터 규격 (마치, 옵시디언에서 메타데이터의 타입을 제한하고 싶을 때처럼,
+#     프론트메터로 지정하면, 데이터에 대한 검증을 쉽게 변경할 수 있다.)
+NameType = Annotated[str, AfterValidator(str.strip), Field(min_length=1)]
+AgeType = Annotated[int, Field(ge=0, le=150)]
+
+# 2. 데이터 금형을 정의
 @dataclass
-class Member:
-    name: str
-    age: int
+class Member():
+    name:NameType
+    age:AgeType
 
-    def __post_init__(self):
-        if not isinstance(self.name, str):
-            raise TypeError(f"유효하지 않은 이름입니다. 당신의 입력 : {self.name}")
-        if not 0 < self.age < 150:
-            raise ValueError(f"유효하지 않은 숫자 혹은 숫자범위입니다. 당신의 입력 : {self.age}")
-        self.name = self.name.strip()
-
-test1 = Member(" 공욱재   ", 25)
-# test2 = Member("공욱재", -5)
-test3 = Member(["공욱재"], 25)
-
-print(test1) # Member(name='공욱재', age=25)
-# print(test2) # ValueError: 유효하지 않은 숫자 혹은 숫자범위입니다. 당신의 입력 : -5
-print(test3) # TypeError: 유효하지 않은 이름입니다. 당신의 입력 : ['공욱재']
+# 3. 데이터 금형을 활용
+try:
+    print(Member("    공욱재  ", 25)) # Member(name='공욱재', age=25)
+except Exception as e:
+    print(f"올바른 데이터 형식이 아닙니다. {e}")
