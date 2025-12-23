cb09ffe90b1d89a0e5f9817270ab7e386a975f77
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-23 17:11:02 +0900

    클래스 기본 테스트

--- 20251223/main.py
+++ 20251223/main.py
@@ -1,1 +1,14 @@
-print("hi")
+from dataclasses import dataclass
+
+@dataclass
+class CafeMenu:
+    name:str
+    price:int
+
+    def order(self):
+        print(f"메뉴 : {self.name}의 가격은 {self.price}원 입니다.")
+    
+cola = CafeMenu("아이스아메리카노", 2000)
+cola.order() # CafeMenu(name='아이스아메리카노', price=2000)
+print(cola) # dataclass __str__ 자동생성 # CafeMenu(name='아이스아메리카노', price=2000)
+print(cola.name) # "아이스 아메리카노"
