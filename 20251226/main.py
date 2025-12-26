cc3d83636d4fa7f82d36da3a2b9e26ec1a47e49f
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-26 16:21:03 +0900

    delete_board() add parameter to choice file

--- 20251226/main.py
+++ 20251226/main.py
@@ -17,16 +17,13 @@ class Board():
         return open (self.file_name, 'r', encoding='utf-8').read()
 
     def update_board(self, content):
-        # 어떤 파일을 업데이터 할 건지 정하고, 내용을 추가하게 할래.
-        # 매개변수에 파일인지 내용인지 구분하게 해서 헷갈리지 않게 할래.
         return open (self.file_name, 'a', encoding='utf-8').write(content + "\n")
 
-    def delete_board(self):
-        # 어떤 것을 삭제할 건지 정하고, 그 파일을 삭제하게 할래.
-        # 테스트 케이스에 파일이름을 할당하게 하고 싶지 않아. 그건 리터럴이야.
-        # 최소한 사람이 어떤 파일을 삭제하는지 직접 입력하게 할래.
-        # 근데 있지도 않은 것을 삭제하게 하면 에러가 나니까 방어하고 싶어.
-        os.remove(self.file_name)
+    def delete_board(self, file_name):
+        if os.path.exists(file_name):
+            os.remove(file_name)
+        else:
+            print("파일이 존재하지 않습니다.")
 
 if __name__ == "__main__":
 
@@ -34,9 +31,9 @@ if __name__ == "__main__":
     board = Board("board.txt")
     # board.create_board() # 그냥 빈 파일 생성
 
-    msg = input("메세지를 입력하세요: ")
-    board.update_board(msg)
+    # msg = input("메세지를 입력하세요: ")
+    # board.update_board(msg)
 
     # print(board.read_board()) # 파일 읽기
 
-    # board.delete_board() # 파일 삭제
+    board.delete_board("test.txt") # 파일 삭제
