a41bd42c0262b3575deb98f44856403d60728b8e
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-30 10:32:23 +0900

    글도 넣기

--- 20251230/main.py
+++ 20251230/main.py
@@ -2,4 +2,11 @@ import tkinter as tk
 
 # 왜 굳이 tk라는 메서드를 호출했을까?
 root = tk.Tk()
+# 왜 매개변수 자리에 = 할당연산자가 있는거지?
+label = tk.Label(root, text="Hello, Tkinter!")
+label.pack(pady=20)
+
+# canvas = tk.Canvas
+# canvas.create_text()
+# 왜 메서드 이름이 loop라는 뜻이 담겨 있을까?
 root.mainloop()
