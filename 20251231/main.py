4566fd244be0b4eb0e7af038bbf89a014a110f59
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2025-12-31 13:00:08 +0900

    now.timestamp()

--- 20251231/main.py
+++ 20251231/main.py
@@ -3,4 +3,8 @@ import datetime
 # 현재 날짜와 시각 객체 생성
 now = datetime.datetime.now() # 모듈.클래스.메소드
 
-print(now)
+# 타임스탬프 어떻게 작성하지?
+timestamp = now.timestamp()
+
+print(timestamp) # 1,767,153,516.3203
+
