ae224cb6fb946ba1533833fa3bdbec96ad4e08a6
ClarusIubar <iubar@duck.com>
2026-01-01 19:43:14 +0900

    storage.py

---
+++ 20260101/storage.py
@@ -0,0 +1,11 @@
+
+import time
+import random
+
+class LottoStorage:
+    def __init__(self):
+        self.records = []
+    def save(self, nums):
+        self.records.append({"id": len(self.records)+1, "nums": nums, "time": time.strftime("%H:%M:%S")})
+    def get_random_5(self):
+        return random.sample(self.records, min(len(self.records), 5))
