65bba992d04da4a7e2eecf4b21f94db0c43e8736
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2026-01-02 12:43:58 +0900

    ball object pseudo-code

---
+++ 20260102/ball.py
@@ -0,0 +1,20 @@
+
+class LottoBall:
+    def __init__(self, temp): # 생성자
+        self.temp = temp
+
+    # 볼 위치
+    def position(self):
+        return
+    
+    # 볼 색상
+    def color(self):
+        return
+
+    # 볼 움직임
+    def move(self):
+        return
+
+    # 볼 충돌
+    def collision(self):
+        return
