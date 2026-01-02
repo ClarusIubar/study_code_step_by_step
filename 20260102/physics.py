d91de46c8fc254036de3839b11a2170f4a4d6594
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2026-01-02 12:57:19 +0900

    physics init

---
+++ 20260102/physics.py
@@ -0,0 +1,16 @@
+
+class Physics_Core:
+    def __init__(self, temp):
+        self.temp = temp
+
+    # 충돌 연산
+    def collision(self):
+        return
+
+    # 중력 연산
+    def gravity(self):
+        return
+
+    # 벽면 제한
+    def wall_limit(self):
+        return
