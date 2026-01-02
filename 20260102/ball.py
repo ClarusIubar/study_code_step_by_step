a83c801698ecbae3bb5ed35938474a658fa481ed
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2026-01-02 13:04:22 +0900

    ball pseudo-code

--- 20260102/ball.py
+++ 20260102/ball.py
@@ -6,15 +6,19 @@ class LottoBall:
     # 볼 위치
     def position(self):
         return
+    # position(pos) 2차원이니까, x,y가 필요함.
     
     # 볼 색상
     def color(self):
         return
+    # if조건에 따라서 볼의 색상을 다르게 함.
 
     # 볼 움직임
     def move(self):
         return
+    # x,y <- physics 연동.
 
     # 볼 충돌
     def collision(self):
         return
+    # x,y끼리 좌표를 확인해서 속도를 변환.
