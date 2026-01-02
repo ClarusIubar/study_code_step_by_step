1ff8816c6d7709f6b39797b304f72de1bb4e3644
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2026-01-02 13:10:14 +0900

    app pseudo-code: what must have been represented?

--- 20260102/app.py
+++ 20260102/app.py
@@ -4,3 +4,19 @@ class LottoApp:
     def __init__(self, root):
         self.root = root
         self.root.title("모든 것이 랜덤인 로또")
+
+    # 동작 버튼(사용자 동작)
+    def user_press_button():
+        return
+
+    # 볼 객체 생성(그리기)
+    def generate_ball():
+        return
+
+    # 볼 움직임 제어(움직임)
+    def controll_ball():
+        return
+
+    # 팝업 생성(결과 표출)
+    def pop_up():
+        return
