7ab8dd5563d1315c4c6802097e432893194fa5c3
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2026-01-02 12:48:57 +0900

    data area for CRUD init

---
+++ 20260102/storage.py
@@ -0,0 +1,13 @@
+
+class LottoStorage:
+    def __init__(self):
+        self.data = []
+
+    def save(self): # create
+        return
+
+    def read(self): # read
+        return
+    
+    def delete(self): # delete
+        return
