48bb8bfab57fcfbe769bf619b998411f61cf5f1d
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2026-01-09 17:15:53 +0900

    memo comment

--- 20260109/memo.py
+++ 20260109/memo.py
@@ -1,25 +1,27 @@
 class MemoManager:
     def __init__(self):
-        self.memos = {i: "" for i in range(1, 10)}
+        self.memos = {i: "" for i in range(1, 10)} # 미리 가용 영역을 할당.
 
-    def create_memo(self, mid, title):
-        if mid in self.memos:
-            self.memos[mid] = title
+    # 혹시 모르니 id(배열을 직접 맵핑)하고 제목만 입력하게 한다. 제목과 내용이 분리된 형태가 아닌 포스트잇 형태의 메모.
+    def create_memo(self, id, title): 
+        if id in self.memos:
+            self.memos[id] = title
             return title
         return None
 
+    # 고정 할당된 영역에 상시 표시
     def read_memos(self):
         return self.memos
 
-    def update_memo(self, mid, title):
-        if mid in self.memos:
-            self.memos[mid] = title
+    def update_memo(self, id, title):
+        if id in self.memos:
+            self.memos[id] = title
             return title
         return None
 
-    def delete_memo(self, mid):
-        if mid in self.memos:
-            self.memos[mid] = ""
+    def delete_memo(self, id):
+        if id in self.memos:
+            self.memos[id] = "" # 빈 문자열로 바꾼다. # 영역 내의 문자들을 지울 뿐.
             return True
         return False
 
